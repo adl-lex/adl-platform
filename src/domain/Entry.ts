f0f5c6d5d44bc0cac16ea4add91fca0821ff6a1b
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2025-11-27 23:03:29 +0100

    add bwb entry types

---
+++ src/domain/Entry.ts
@@ -0,0 +1,41 @@
+export interface Entry {
+  id: string
+  form?: Form
+  gramGrp?: GrammarGroup
+  sense?: Sense[]
+}
+
+export interface Form {
+  type: string
+  orth: string
+  hyph: string[]
+  pron: string
+  form?: Form
+}
+
+export type GrammarType =
+  | "pos"
+  | "case"
+  | "gender"
+  | "inflectionType"
+  | "mood"
+  | "number"
+  | "person"
+  | "tense"
+  | "colloc"
+  | "aspect"
+  | "valency"
+  | "government"
+  | "degree"
+
+export interface GrammarGroup {
+  gram: Record<GrammarType, string>
+}
+
+export interface Sense {
+  id: string
+  n: string
+  gloss: string
+  def: string
+  sense?: Sense[]
+}
