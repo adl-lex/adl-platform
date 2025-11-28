d1ec17c007cab6eecfa2bc9d2749d45dc4a06d94
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2025-11-28 20:04:41 +0100

    add lemma search form

--- src/pages/SearchDisplay.tsx
+++ src/pages/SearchDisplay.tsx
@@ -1,7 +1,21 @@
+import { ActionIcon, Box, TextInput } from "@mantine/core"
+import { IconSearch } from "@tabler/icons-react"
+
 export default function SearchDisplay() {
+  const SearchButton = (
+    <ActionIcon variant="white" size="lg">
+      <IconSearch size={16} />
+    </ActionIcon>
+  )
   return (
-    <>
-      <h1>Search Page</h1>
-    </>
+    <Box maw="800px" mx="auto" py="xl" px="md">
+      <h1>Lemmasuche</h1>
+
+      <TextInput
+        rightSection={SearchButton}
+        flex={1}
+        placeholder="Wörterbücher durchsuchen..."
+      />
+    </Box>
   )
 }
