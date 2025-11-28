e672f08b7079d95263199cfb921083707504207b
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2025-11-28 20:25:32 +0100

    extract LemmaSearchForm component

--- src/pages/SearchDisplay.tsx
+++ src/pages/SearchDisplay.tsx
@@ -1,38 +1,12 @@
-import { ActionIcon, Box, Group, TextInput } from "@mantine/core"
-import { IconSearch } from "@tabler/icons-react"
-import { useNavigate } from "react-router-dom"
-import { useState } from "react"
+import { Box } from "@mantine/core"
+import LemmaSearchForm from "../ui/LemmaSearchForm"
 
 export default function SearchDisplay() {
-  const navigate = useNavigate()
-  const [searchValue, setSearchValue] = useState("")
-
-  const SearchButton = (
-    <ActionIcon variant="white" size="lg">
-      <IconSearch size={16} />
-    </ActionIcon>
-  )
-
-  const handleSubmit = (e: React.FormEvent<HTMLDivElement>) => {
-    e.preventDefault()
-    if (searchValue.trim()) {
-      navigate(`/search/lemma/${encodeURIComponent(searchValue.trim())}`)
-    }
-  }
-
   return (
     <Box maw="800px" mx="auto" py="xl" px="md">
       <h1>Stichwortsuche</h1>
 
-      <Group component={"form"} onSubmit={handleSubmit}>
-        <TextInput
-          rightSection={SearchButton}
-          flex={1}
-          placeholder="Stichwortsuche..."
-          value={searchValue}
-          onChange={(e) => setSearchValue(e.currentTarget.value)}
-        />
-      </Group>
+      <LemmaSearchForm />
     </Box>
   )
 }
---
+++ src/ui/LemmaSearchForm.tsx
@@ -0,0 +1,34 @@
+import { ActionIcon, Group, TextInput } from "@mantine/core"
+import { IconSearch } from "@tabler/icons-react"
+import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+
+export default function SearchDisplay() {
+  const navigate = useNavigate()
+  const [searchValue, setSearchValue] = useState("")
+
+  const SearchButton = (
+    <ActionIcon variant="white" size="lg">
+      <IconSearch size={16} />
+    </ActionIcon>
+  )
+
+  const handleSubmit = (e: React.FormEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    if (searchValue.trim()) {
+      navigate(`/search/lemma/${encodeURIComponent(searchValue.trim())}`)
+    }
+  }
+
+  return (
+    <Group component={"form"} onSubmit={handleSubmit}>
+      <TextInput
+        rightSection={SearchButton}
+        flex={1}
+        placeholder="Stichwortsuche..."
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.currentTarget.value)}
+      />
+    </Group>
+  )
+}
