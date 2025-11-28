1092a66f6750367a0ac854b8c54972308154ea07
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2025-11-28 20:18:52 +0100

    add lemma search form

--- src/pages/SearchDisplay.tsx
+++ src/pages/SearchDisplay.tsx
@@ -1,21 +1,38 @@
-import { ActionIcon, Box, TextInput } from "@mantine/core"
+import { ActionIcon, Box, Group, TextInput } from "@mantine/core"
 import { IconSearch } from "@tabler/icons-react"
+import { useNavigate } from "react-router-dom"
+import { useState } from "react"
 
 export default function SearchDisplay() {
+  const navigate = useNavigate()
+  const [searchValue, setSearchValue] = useState("")
+
   const SearchButton = (
     <ActionIcon variant="white" size="lg">
       <IconSearch size={16} />
     </ActionIcon>
   )
+
+  const handleSubmit = (e: React.FormEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    if (searchValue.trim()) {
+      navigate(`/search/lemma/${encodeURIComponent(searchValue.trim())}`)
+    }
+  }
+
   return (
     <Box maw="800px" mx="auto" py="xl" px="md">
-      <h1>Lemmasuche</h1>
+      <h1>Stichwortsuche</h1>
 
-      <TextInput
-        rightSection={SearchButton}
-        flex={1}
-        placeholder="Wörterbücher durchsuchen..."
-      />
+      <Group component={"form"} onSubmit={handleSubmit}>
+        <TextInput
+          rightSection={SearchButton}
+          flex={1}
+          placeholder="Stichwortsuche..."
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.currentTarget.value)}
+        />
+      </Group>
     </Box>
   )
 }
