30f195ba95c631686888d8c8eda72e12ec777628
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2025-12-23 22:49:17 +0100

    simplify empty result display

--- src/ui/LemmaDisplay.tsx
+++ src/ui/LemmaDisplay.tsx
@@ -2,16 +2,13 @@ import _ from "lodash"
 import { JSX } from "react"
 import { DisplayEntry } from "../domain/Entry"
 import { Text, Card } from "@mantine/core"
-import { useParams } from "react-router-dom"
 import { DisplayVariants, EntryHeader } from "./SearchResult"
 import DisplaySense from "./DisplaySense"
 
 export function LemmaNotFound(): JSX.Element {
-  const { id } = useParams<{ id: string }>()
-
   return (
     <section>
-      <Text>Lemma "{id}" nicht gefunden.</Text>
+      <Text>Keine Treffer</Text>
     </section>
   )
 }
